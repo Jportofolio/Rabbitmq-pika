38c780f1ae53a6a329305ab6efce2f9a7302232e
Jportofolio <koolmx9@gmail.com>
2018-08-12 21:19:57 -0500

    adding basic_qos with prefetch_count =1

--- worker.py
+++ worker.py
@@ -20,7 +20,9 @@ def callback(ch, method, properties, body):
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
-channel.basic_consume(callback, queue='hello')
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(callback, queue='task_queue')
+
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
